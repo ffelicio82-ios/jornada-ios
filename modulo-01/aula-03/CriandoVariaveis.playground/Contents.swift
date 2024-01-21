d3626d53ea3cfb43392571c20358def4ee8dbdc7
Fernando da Costa Felício <99772921+ffelicio82@users.noreply.github.com>
2024-01-21 00:21:16 -0300

    feat: estudo das variaveis (#1)

---
+++ modulo-01/aula-03/CriandoVariaveis.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// adicionando comentário
+
+// 1 - var -> variável
+var nome : String = "Fernando" // tipagem explicita
+var sobrenome = "Felício" // tipagem implícita - a linguagem infere automaticamente
+
+print(nome)
+print(sobrenome)
